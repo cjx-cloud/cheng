ede129dfacbcd16c21e29781bddcf4c98649a895
cjx-cloud <1741868282@qq.com>
2026-04-15 18:49:20 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,36 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include<QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+  if(ui->pushButton->text()=="开灯"){
+
+      myled.on(fsmpLeds::LED1);
+      ui->pushButton->setText("关灯");
+  }else{
+      myled.off(fsmpLeds::LED1);
+      ui->pushButton->setText("开灯");
+  }
+}
+
+
+void Widget::on_pushButton_2_clicked()
+{
+    qDebug() << "今晚吃酸辣粉" <<endl;
+
+}
+
